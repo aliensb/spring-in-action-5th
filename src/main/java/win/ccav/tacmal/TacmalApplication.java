5c4fc70824c49a3d4f9e447da33aeec74f6cf04a
tom <tom>
2019-03-21 17:24:40 +0800

    project init

---
+++ src/main/java/win/ccav/tacmal/TacmalApplication.java
@@ -0,0 +1,13 @@
+package win.ccav.tacmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacmalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacmalApplication.class, args);
+    }
+
+}
